ce5c36b0f7a31a144a0d238182eaf65514d2a380
thebournegenius <theamit97@gmail.com>
2016-10-16 10:54:35 +0530

    Add files via upload

---
+++ C/sizeofc.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<limits.h>
+
+void main()
+{
+    printf("Int: %d\n", sizeof(int));
+    printf("Float: %d\n", sizeof(float));
+    printf("Char: %d\n", sizeof(char));
+    printf("Double: %d\n", sizeof(double));
+
+}
